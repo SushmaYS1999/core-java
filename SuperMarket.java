3fdf6f8fb325ac4db85a508d804a7e59475ab908
SushmaYS1999 <sushmagowda.xworkz@gmail.com>
2022-09-22 13:07:54 +0530

    java files

---
+++ SuperMarket.java
@@ -0,0 +1,131 @@
+class SuperMarket{
+
+	static void getVegatables(String[] vegatables){
+	System.out.println("inside getVegatables()");
+	System.out.println("size of vegatables " + vegatables.length);
+	System.out.println("List of Vegatables are");
+	for(int i = 0;i<vegatables.length;i++)
+	{
+		System.out.println(vegatables[i]);
+	}
+
+		System.out.println("end of Vegatables()" );
+		System.out.println("************");
+	}
+	
+	static void getPerfumes(String[] perfumes){
+	System.out.println("inside getPerfumes()");
+	System.out.println("number of Perfumes " + perfumes.length);
+	System.out.println("List of Perfumes are");
+	for(int j=0; j < perfumes.length;j++)
+	{
+		System.out.println(perfumes[j]);
+	}
+	
+	System.out.println("end of perfumes()" );
+	System.out.println("************");
+	}
+	
+	
+	static void getBiscuites(String[] biscuites){
+	System.out.println("inside getBiscuites");
+	System.out.println("size of biscuites " +biscuites.length);
+	System.out.println("List of biscuites are");
+	for(int k=0;k < biscuites.length;k++)
+	{
+		System.out.println(biscuites[k]);
+	}
+	System.out.println("end of biscuites");
+	System.out.println("************");
+	}
+	static void getFruits(String[] fruits){
+	System.out.println("inside getFruits");
+	System.out.println("size of fruits " +fruits.length);
+	System.out.println("List of fruits are");
+	for(int m=0;m < fruits.length;m++)
+	{
+		System.out.println(fruits[m]);
+	}
+	System.out.println("end of fruits");
+	System.out.println("************");
+	}
+	static void getChocolates(String[] chocolagtes){
+	System.out.println("inside getChocolates");
+	System.out.println("size of chocolagtes " +chocolagtes.length);
+	System.out.println("List of chocolagtes are");
+	for(int n=0;n < chocolagtes.length;n++)
+	{
+		System.out.println(chocolagtes[n]);
+	}
+	System.out.println("end of chocolagtes");
+	System.out.println("************");
+	}
+	static void getGroceries(String[] groceries){
+	System.out.println("inside getGroceries");
+	System.out.println("size of groceries " +groceries.length);
+	System.out.println("List of groceries are");
+	for(int p=0;p < groceries.length;p++)
+	{
+		System.out.println(groceries[p]);
+	}
+	System.out.println("end of groceries");
+	System.out.println("************");
+	}
+	static void getMeat(String[] meat){
+	System.out.println("inside getMeat");
+	System.out.println("size of meat " +meat.length);
+	System.out.println("List of meat are");
+	for(int q=0;q < meat.length;q++)
+	{
+		System.out.println(meat[q]);
+	}
+	System.out.println("end of meat");
+	System.out.println("************");
+	}
+	static void getCosmatics(String[] cosmatics){
+	System.out.println("inside getCosmatics");
+	System.out.println("size of cosmatics " +cosmatics.length);
+	System.out.println("List of cosmatics are");
+	for(int r=0;r < cosmatics.length;r++)
+	{
+		System.out.println(cosmatics[r]);
+	}
+	System.out.println("end of cosmatics");
+	System.out.println("************");
+	}
+	static void getShoes(String[] shoes){
+	System.out.println("inside getShoes");
+	System.out.println("size of shoes " +shoes.length);
+	System.out.println("List of shoes are");
+	for(int u=0;u < shoes.length;u++)
+	{
+		System.out.println(shoes[u]);
+	}
+	System.out.println("end of shoes");
+	System.out.println("************");
+	}
+	static void getElectronicDevices(String[] electronicDevices){
+	System.out.println("inside getElectronicDevices");
+	System.out.println("size of electronicDevices " +electronicDevices.length);
+	System.out.println("List of electronicDevices are");
+	for(int v=0;v < electronicDevices.length;v++)
+	{
+		System.out.println(electronicDevices[v]);
+	}
+	System.out.println("end of electronicDevices");
+	System.out.println("************");
+	}
+	static void getClothes(String[] clothes){
+	System.out.println("inside getClothes");
+	System.out.println("size of clothes " +clothes.length);
+	System.out.println("List of clothes are");
+	for(int x=0;x < clothes.length;x++)
+	{
+		System.out.println(clothes[x]);
+	}
+	System.out.println("end of clothes");
+	System.out.println("************");
+	}
+
+
+}
--- Temple.java
+++
@@ -1,5 +0,0 @@
-class Temple{
-	public static void main(String args[]){
-		System.out.println("This place gives a positive vibes");
-	}
-}
